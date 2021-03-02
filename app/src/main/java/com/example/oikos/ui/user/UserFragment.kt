cc30331cf8fb8fedbcf4231e6718d10d4122df8f
airam <airamhr7@gmail.com>
2021-03-02 20:45:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/oikos/ui/user/UserFragment.kt
@@ -0,0 +1,31 @@
+package com.example.oikos.ui.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.oikos.R
+
+class UserFragment : Fragment() {
+
+    private lateinit var userViewModel: UserViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        userViewModel =
+                ViewModelProvider(this).get(UserViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        userViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
