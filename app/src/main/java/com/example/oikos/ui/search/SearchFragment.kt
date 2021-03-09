fe6c756fd0e9732490f6640a4552b9b14689961f
airam <airamhr7@gmail.com>
2021-03-09 17:27:33 +0000

    Hecha tarjeta_inmueble.xml para búsquedas y primeros pasos para búsqueda geolocalizada

--- app/src/main/java/com/example/oikos/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/oikos/ui/search/SearchFragment.kt
@@ -7,7 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.oikos.R
+import com.here.sdk.core.Color
+import com.here.sdk.core.GeoCircle
 import com.here.sdk.core.GeoCoordinates
+import com.here.sdk.core.GeoPolygon
+import com.here.sdk.mapview.MapPolygon
 import com.here.sdk.mapview.MapScheme
 import com.here.sdk.mapview.MapView
 
@@ -35,6 +39,9 @@ class SearchFragment : Fragment() {
             // It will not be called before the first map scene was loaded.
             // Any code that requires map data may not work as expected beforehand.
             println("Map ready")
+            val coordList = listOf<GeoCoordinates>(GeoCoordinates(52.530932, 13.384915), GeoCoordinates(52.520932, 13.384915), GeoCoordinates(52.530932, 13.394915), GeoCoordinates(52.550932, 13.39915),
+                    GeoCoordinates(52.560932, 13.44915), GeoCoordinates(52.580932, 13.384915), GeoCoordinates(52.530932, 13.404915), GeoCoordinates(52.530932, 13.584915))
+            drawCircles(coordList, mapView)
         }
         return root
     }
@@ -50,6 +57,21 @@ class SearchFragment : Fragment() {
                 println("Loading map failed")
             }
         }
+    }
+
+    fun drawCircles(coordList: List<GeoCoordinates>, mapView: MapView){
+        for(coord in coordList){
+            mapView.mapScene.addMapPolygon(createMapCircle(coord))
+        }
+    }
+
+    fun createMapCircle(coordinates : GeoCoordinates) : MapPolygon{
+        val radiusInMeters = 150.0
+        val geoCircle = GeoCircle(GeoCoordinates(coordinates.latitude, coordinates.longitude), radiusInMeters);
+
+        val geoPolygon = GeoPolygon(geoCircle);
+        val fillColor : Color = Color.valueOf(0.5f, 0.39f, 0.71f, 0.96f) // RGBA
+        return MapPolygon(geoPolygon, fillColor)
     }
 
     override fun onPause() {
