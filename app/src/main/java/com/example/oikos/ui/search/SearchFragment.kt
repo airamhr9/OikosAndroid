a1783334f0a545cddbf97c5ce9c7671ebbdfef9a
airam <airamhr7@gmail.com>
2021-03-09 21:22:44 +0000

    Hecho que al hacer click en un marcador del mapa salga una tarjeta, y al clickar fuera desaparezca.
    Añadidos marcadores al mapa junto con los círculos

--- app/src/main/java/com/example/oikos/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/oikos/ui/search/SearchFragment.kt
@@ -4,22 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.oikos.R
-import com.here.sdk.core.Color
-import com.here.sdk.core.GeoCircle
-import com.here.sdk.core.GeoCoordinates
-import com.here.sdk.core.GeoPolygon
-import com.here.sdk.mapview.MapPolygon
-import com.here.sdk.mapview.MapScheme
-import com.here.sdk.mapview.MapView
+import com.here.sdk.core.*
+import com.here.sdk.gestures.TapListener
+import com.here.sdk.mapview.*
 
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
     private lateinit var mapView : MapView
+    private lateinit var mapCard : CardView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -30,24 +29,22 @@ class SearchFragment : Fragment() {
                 ViewModelProvider(this).get(SearchViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_search, container, false)
 
+        mapCard = root.findViewById(R.id.map_card)
 
         mapView = root.findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         loadMapScene()
         mapView.setOnReadyListener {
-            // This will be called each time after this activity is resumed.
-            // It will not be called before the first map scene was loaded.
-            // Any code that requires map data may not work as expected beforehand.
-            println("Map ready")
-            val coordList = listOf<GeoCoordinates>(GeoCoordinates(52.530932, 13.384915), GeoCoordinates(52.520932, 13.384915), GeoCoordinates(52.530932, 13.394915), GeoCoordinates(52.550932, 13.39915),
+            setTapGestureHandler()
+            val coordList = listOf(GeoCoordinates(52.530932, 13.384915), GeoCoordinates(52.520932, 13.384915), GeoCoordinates(52.530932, 13.394915), GeoCoordinates(52.550932, 13.39915),
                     GeoCoordinates(52.560932, 13.44915), GeoCoordinates(52.580932, 13.384915), GeoCoordinates(52.530932, 13.404915), GeoCoordinates(52.530932, 13.584915))
-            drawCircles(coordList, mapView)
+            drawCircles(coordList)
         }
+
         return root
     }
 
     private fun loadMapScene() {
-        // Load a scene from the HERE SDK to render the map with a map scheme.
         mapView.mapScene.loadScene(MapScheme.NORMAL_DAY) { mapError ->
             if (mapError == null) {
                 val distanceInMeters = (1000 * 10).toDouble()
@@ -59,19 +56,50 @@ class SearchFragment : Fragment() {
         }
     }
 
-    fun drawCircles(coordList: List<GeoCoordinates>, mapView: MapView){
+    private fun drawCircles(coordList: List<GeoCoordinates>){
+        val mapImage = MapImageFactory.fromResource(context?.resources, R.drawable.map_marker)
         for(coord in coordList){
             mapView.mapScene.addMapPolygon(createMapCircle(coord))
+            val mapMarker = MapMarker(coord, mapImage)
+            mapView.mapScene.addMapMarker(mapMarker)
         }
     }
 
-    fun createMapCircle(coordinates : GeoCoordinates) : MapPolygon{
-        val radiusInMeters = 150.0
+    private fun createMapCircle(coordinates: GeoCoordinates) : MapPolygon{
+        val radiusInMeters = 250.0
         val geoCircle = GeoCircle(GeoCoordinates(coordinates.latitude, coordinates.longitude), radiusInMeters);
-
         val geoPolygon = GeoPolygon(geoCircle);
-        val fillColor : Color = Color.valueOf(0.5f, 0.39f, 0.71f, 0.96f) // RGBA
-        return MapPolygon(geoPolygon, fillColor)
+        val fillColor : Color = Color.valueOf(0.827f, 0.184f, 0.184f, 0.5f) // RGBA
+       return MapPolygon(geoPolygon, fillColor)
+    }
+
+    private fun setTapGestureHandler() {
+        mapView.gestures.tapListener = TapListener { touchPoint -> pickMapMarker(touchPoint) }
+    }
+
+    private fun pickMapMarker(touchPoint: Point2D) {
+        val radiusInPixel = 2f
+        mapView.pickMapItems(touchPoint, radiusInPixel.toDouble()) {
+            if(it != null)
+                onPickMapItems(it)
+        }
+    }
+
+    private fun onPickMapItems(pickMapItemsResult: PickMapItemsResult) {
+        val mapMarkerList = pickMapItemsResult.markers
+        if (mapMarkerList.size == 0) {
+            slideDown(mapCard)
+            mapCard.visibility = View.INVISIBLE
+            return
+        }
+        val topmostMapMarker = mapMarkerList[0]
+        //JColorChooser.showDialog("Map marker picked:", "Location: " +
+        //      topmostMapMarker.coordinates.latitude + ", " +
+        //    topmostMapMarker.coordinates.longitude)
+        if (mapCard.visibility == View.INVISIBLE){
+            mapCard.visibility = View.VISIBLE
+            slideUp(mapCard)
+        }
     }
 
     override fun onPause() {
@@ -89,5 +117,27 @@ class SearchFragment : Fragment() {
         mapView.onDestroy()
     }
 
+    private fun slideUp(view: View) {
+        view.visibility = View.VISIBLE
+        val animate = TranslateAnimation(
+                0f,  // fromXDelta
+                0f,  // toXDelta
+                view.height.toFloat(),  // fromYDelta
+                0f) // toYDelta
+        animate.duration = 100
+        animate.fillAfter = true
+        view.startAnimation(animate)
+    }
+
+    // slide the view from its current position to below itself
+    private fun slideDown(view: View) {
+        val animate = TranslateAnimation(
+                0f,  // fromXDelta
+                0f,  // toXDelta
+                0f,  // fromYDelta
+                view.height.toFloat()) // toYDelta
+        animate.duration = 100
+        view.startAnimation(animate)
+    }
 
 }
