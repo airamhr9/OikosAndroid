7342fbfb3c57191523612438b33c2fb77ecd0220
airam <airamhr7@gmail.com>
2021-04-17 23:02:25 +0100

    Hechas interfaces de gestión de inmuebles y la nueva búsqueda

--- app/src/main/java/com/example/oikos/MainActivity.kt
+++ app/src/main/java/com/example/oikos/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController        // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_user))
+                R.id.navigation_home, R.id.navigation_gestion_inmueble, R.id.navigation_search, R.id.navigation_user))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
