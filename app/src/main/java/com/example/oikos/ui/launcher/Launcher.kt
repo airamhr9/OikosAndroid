c38ed272e9f35960da8452205ee2c2928808bf06
Jaime <jnegrillopastor@hotmail.com>
2021-04-26 17:26:54 +0200

    Parte de login

---
+++ app/src/main/java/com/example/oikos/ui/launcher/Launcher.kt
@@ -0,0 +1,12 @@
+package com.example.oikos.ui.launcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.oikos.R
+
+class Launcher : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
