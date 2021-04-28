dcaaa85b32fdcac7ce3c16f565e7043150a74ecb
Jaime <jnegrillopastor@hotmail.com>
2021-04-28 13:12:34 +0200

    Login y Registro diseñados

---
+++ app/src/main/java/com/example/oikos/ui/user/registro.kt
@@ -0,0 +1,12 @@
+package com.example.oikos.ui.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.oikos.R
+
+class registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
